532e780d8f39959c4071c2d39411a91c741b9b28
Andrés Chávez González <a_ch97@hotmail.com>
2024-11-20 16:46:56 +0100

    Modificación del FXML

---
+++ src/main/java/buscahipoteeca/app/App.java
@@ -0,0 +1,39 @@
+package buscahipoteeca.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+	 @Override
+	    public void start(Stage primaryStage) {
+	        try {
+	            // Cargamos el FXML -> src/main/resources/VIEW/MainView.fxml
+	            FXMLLoader loader = new FXMLLoader(getClass().getResource("/VIEW/FXML/MainView.fxml"));
+	            Parent root = loader.load();
+
+	            // Crear el objecto scene (La vista)
+	            Scene scene = new Scene(root);
+
+	            // El título de la ventana (Se puede dejar vacio)
+	            primaryStage.setTitle("Buscahipoteeca");
+
+	            // Ícono para la ventana (Opcional)
+	            primaryStage.getIcons().add(
+	            	    new javafx.scene.image.Image(getClass().getResourceAsStream("/VIEW/IMAGENES/logo.png"))
+	            	);
+	            // Mostrar la vista
+	            primaryStage.setScene(scene);
+	            primaryStage.show();
+	        } catch (Exception e) {
+	        	// Mostramos el error
+	            e.printStackTrace();
+	        }
+	    }
+	 public static void main(String[] args) {
+	        launch(args);
+	    }
+}
